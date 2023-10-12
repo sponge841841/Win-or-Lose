f35aac5597cc02153163b922e0830b1f37b21483
sponge841841 <a10121582@gmail.com>
2023-10-12 21:39:36 +0900

    🎉 first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# Flaskアプリの初期化
+app = Flask(__name__)
+
+# ルートURLの処理
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# アプリの起動
+if __name__ == '__main__':
+    app.run(debug=True)
